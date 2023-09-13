e2f865d7d47dcb7774d4d0ad053763f37eda9922
jayashreebalinani <jayashreebalinani@gmail.com>
2023-09-13 22:23:48 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,121 @@
+document.getElementById("12").value = 5;
+document.getElementById("12").readOnly= true;
+
+document.getElementById("13").value = 2;
+document.getElementById("13").readOnly= true;
+
+document.getElementById("21").value = 1;
+document.getElementById("21").readOnly= true;
+
+document.getElementById("22").value = 6;
+document.getElementById("22").readOnly= true;
+
+document.getElementById("32").value = 4;
+document.getElementById("32").readOnly= true;
+
+document.getElementById("33").value = 9;
+document.getElementById("33").readOnly= true;
+
+document.getElementById("16").value = 6;
+document.getElementById("16").readOnly= true;
+
+document.getElementById("24").value = 9;
+document.getElementById("24").readOnly= true;
+
+document.getElementById("34").value = 8;
+document.getElementById("34").readOnly= true;
+
+document.getElementById("36").value = 3;
+document.getElementById("36").readOnly= true;
+
+document.getElementById("29").value = 4;
+document.getElementById("29").readOnly= true;
+
+document.getElementById("37").value = 6;
+document.getElementById("37").readOnly= true;
+
+//document.getElementById("38").value = 2;
+// document.getElementById("38").readOnly= true;
+
+document.getElementById("41").value = 4;
+document.getElementById("41").readOnly= true;
+
+document.getElementById("52").value = 8;
+document.getElementById("52").readOnly= true;
+
+document.getElementById("53").value = 3;
+document.getElementById("53").readOnly= true;
+
+document.getElementById("64").value = 1;
+document.getElementById("64").readOnly= true;
+
+document.getElementById("40").value = 2;
+document.getElementById("40").readOnly= true;
+
+document.getElementById("54").value = 1;
+document.getElementById("54").readOnly= true;
+
+document.getElementById("47").value = 8;
+document.getElementById("47").readOnly= true;
+
+document.getElementById("57").value = 5;
+document.getElementById("57").readOnly= true;
+
+document.getElementById("58").value = 9;
+document.getElementById("58").readOnly= true;
+
+document.getElementById("69").value = 2;
+document.getElementById("69").readOnly= true;
+
+document.getElementById("72").value = 9;
+document.getElementById("72").readOnly= true;
+
+document.getElementById("73").value = 7;
+document.getElementById("73").readOnly= true;
+
+document.getElementById("81").value = 2;
+document.getElementById("81").readOnly= true;
+
+document.getElementById("74").value = 3;
+document.getElementById("74").readOnly= true;
+
+document.getElementById("94").value = 5;
+document.getElementById("94").readOnly= true;
+
+document.getElementById("86").value = 9;
+document.getElementById("86").readOnly= true;
+
+document.getElementById("94").value = 1;
+document.getElementById("94").readOnly= true;
+
+document.getElementById("77").value = 2;
+document.getElementById("77").readOnly= true;
+
+document.getElementById("78").value = 4;
+document.getElementById("78").readOnly= true;
+
+document.getElementById("88").value = 5;
+document.getElementById("88").readOnly= true;
+
+document.getElementById("89").value = 6;
+document.getElementById("89").readOnly= true;
+
+document.getElementById("87").value = 9;
+document.getElementById("87").readOnly= true;
+
+document.getElementById("98").value = 7;
+document.getElementById("98").readOnly= true;
+
+// function start(){
+//     if()
+// }
+// document.getElementById("21").value = 9;
+
+// for(var i = 1; i < 9; i++)
+// {
+//     for(var j = 1;j < 9; j++)
+//     {
+        
+
+//     }
+// }
