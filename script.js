43be55c917704893106a00a776449c67788c438a
Jayashree Balineni <115000642+jayashreebalinani@users.noreply.github.com>
2023-10-02 19:30:47 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,84 +1,54 @@
-document.getElementById("12").value = 5;
-document.getElementById("12").readOnly= true;
 
 
+let numselected = null
+
+window.onload = function()
+{
+    setgame();
+}
 
 
+function setgame()
+{
+    for(let i = 0;i <= 9;i++)
+    {
+        let number = document.createElement("div")
+        number.id=i
+        number.innerText=i
+        number.addEventListener("click",selectnumber)
+        number.classList.add("number")
+        document.getElementById("digits").append (number)
+    }
+    for(let i = 0;i < 9;i++)
+    {
+        for(let j = 0; j < 9; j++)
+        {
+            let cells = document.createElement("div")
+            cells.id = i+"-"+j
+            cells.addEventListener("click",selectcells)
+            cells.classList.add("cells")
+            document.getElementById("board").append(cells)
+        }
+    }
+}
 
+function selectnumber()
+{
+    
+    if(numselected != null)
+    {
+        numselected.classList.remove("numberselected")
+    }
+  //  numselected.classList.add("numberselected")
+  numselected = this
+    numselected.classList.add("numberselected")
+    
+}
 
-
-
-
-
-
-
-
-
-
-
-
-
-document.getElementById("47").value = 8;
-document.getElementById("47").readOnly= true;
-
-document.getElementById("57").value = 5;
-document.getElementById("57").readOnly= true;
-
-document.getElementById("58").value = 9;
-document.getElementById("58").readOnly= true;
-
-document.getElementById("69").value = 2;
-document.getElementById("69").readOnly= true;
-
-document.getElementById("72").value = 9;
-document.getElementById("72").readOnly= true;
-
-document.getElementById("73").value = 7;
-document.getElementById("73").readOnly= true;
-
-document.getElementById("81").value = 2;
-document.getElementById("81").readOnly= true;
-
-document.getElementById("74").value = 3;
-document.getElementById("74").readOnly= true;
-
-document.getElementById("94").value = 5;
-document.getElementById("94").readOnly= true;
-
-document.getElementById("86").value = 9;
-document.getElementById("86").readOnly= true;
-
-document.getElementById("94").value = 1;
-document.getElementById("94").readOnly= true;
-
-document.getElementById("77").value = 2;
-document.getElementById("77").readOnly= true;
-
-document.getElementById("78").value = 4;
-document.getElementById("78").readOnly= true;
-
-document.getElementById("88").value = 5;
-document.getElementById("88").readOnly= true;
-
-document.getElementById("89").value = 6;
-document.getElementById("89").readOnly= true;
-
-document.getElementById("87").value = 9;
-document.getElementById("87").readOnly= true;
-
-document.getElementById("98").value = 7;
-document.getElementById("98").readOnly= true;
-
-// function start(){
-//     if()
-// }
-// document.getElementById("21").value = 9;
-
-// for(var i = 1; i < 9; i++)
-// {
-//     for(var j = 1;j < 9; j++)
-//     {
-        
-
-//     }
-// }
+function selectcells()
+{
+    if(numselected)
+    {
+        this.innerText = numselected.id;
+    }
+}
