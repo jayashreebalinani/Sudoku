2115c002df1f73905c7214e5004b8d39e0c2c169
Jayashree Balineni <115000642+jayashreebalinani@users.noreply.github.com>
2023-10-03 18:19:07 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,33 @@
 
 
 let numselected = null
+var cellselected = null;
+
+var errors = 0;
+
+var board = [
+    "--74916-5",
+    "2---6-3-9",
+    "-----7-1-",
+    "-586----4",
+    "--3----9-",
+    "--62--187",
+    "9-4-7---2",
+    "67-83----",
+    "81--45---"
+]
+
+var solution = [
+    "387491625",
+    "241568379",
+    "569327418",
+    "758619234",
+    "123784596",
+    "496253187",
+    "934176852",
+    "675832941",
+    "812945763"
+]
 
 window.onload = function()
 {
@@ -25,6 +52,23 @@ function setgame()
         {
             let cells = document.createElement("div")
             cells.id = i+"-"+j
+            if(board[i][j] != "-")
+            {
+                cells.innerText=board[i][j]
+               
+                cells.classList.add("cellsfilled")
+               // document.getElementById(i+"-"+j).readOnly= true;
+
+                
+            }
+            if(i == 2 || i == 5)
+            {
+                cells.classList.add("divider")
+            }
+            if(j == 2||j==5)
+            {
+                cells.classList.add("divider1")
+            }
             cells.addEventListener("click",selectcells)
             cells.classList.add("cells")
             document.getElementById("board").append(cells)
